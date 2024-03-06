6f3e2f4657d6d4ac412613afe6a016713e678780
K SANJU KUMAR <129849456+sanjukumarkanki@users.noreply.github.com>
2024-03-06 06:42:34 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>myPhpProject</title>
+    <script src="https://kit.fontawesome.com/ca3b6e4e89.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header style="box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;" class="bg-white z-10 w-full fixed top-0 left-0 right-0 flex justify-between items-center pl-8 pr-8 pb-1 pt-1 ">
+        <!-- Company Logo -->
+        <img class=" h-20 w-20" src="	https://www.cioncancerclinics.com/biopsy-test-hyderabad/assets/img/v2/cion-cancer-clinics-logo.svg" alt="logo" />
+        <!-- Contact Icon -->
+        
+
+        <button style="box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;" class= " bg-red-500 border-none outline-none text-sm font-bold text-white rounded-md p-2 flex justify-center items-center">
+           <span>Call: </span>
+           <span>+916301156429</span>
+        </button>
+    </header>
+    <?php 
+     echo "sanju"
+    ?>
+</body>
+</html>
